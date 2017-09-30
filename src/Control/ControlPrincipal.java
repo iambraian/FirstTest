9bc5970efb30dd2bd77b94c26709afe9e90f43b7
iambraian <bj026in@gmail.com>
2017-09-30 16:53:44 -0500

    El usuario ya puede especificar los numeros

--- src/Control/ControlPrincipal.java
+++ src/Control/ControlPrincipal.java
@@ -1,9 +1,15 @@
 package Control;
 
+import java.util.Scanner;
+
 class ControlPrincipal{
     public static void main(String[] args) {
-        double num1 = 70;
-        double num2 = 90;
+        
+        System.out.println("Ingrese los numeros a procesar: ");
+        
+        Scanner inputUsuario = new Scanner(System.in);
+        double num1 = inputUsuario.nextDouble();
+        double num2 = inputUsuario.nextDouble();
         
         double suma = num1 + num2;
         System.out.println("SUMA = " + suma);
