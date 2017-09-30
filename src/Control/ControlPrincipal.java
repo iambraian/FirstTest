65ae9a46f4b04051a86ddc52fa530aad7ae43320
iambraian <bj026in@gmail.com>
2017-09-30 15:40:32 -0500

    Commit inicial

---
+++ src/Control/ControlPrincipal.java
@@ -0,0 +1,11 @@
+package Control;
+
+class ControlPrincipal{
+    public static void main(String[] args) {
+        int v1 = 70;
+        int v2 = 90;
+        
+        int suma = v1 + v2;
+        System.out.println("SUMA: " + suma);
+    }
+}
